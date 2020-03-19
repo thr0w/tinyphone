494d262b5b9d083a1eca09712671db67d9e5246a
thr0w <JosefernandoTolentino@gmail.com>
2020-03-19 07:30:10 -0300

    localhost:7117

--- tinyphone/consts.h
+++ tinyphone/consts.h
@@ -14,7 +14,7 @@
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 #define STRING_REMOVE_CHAR(str, ch) str.erase(std::remove(str.begin(), str.end(), ch), str.end())
 
-#define REMOTE_CONFIG_URL "http://localhost/tpconf"
+#define REMOTE_CONFIG_URL "http://localhost:7117/tpconf"
 #define REMOTE_CONFIG_URL_SECONDARY "https://raw.githubusercontent.com/voiceip/tinyphone/%s/config.json"
 
 #define HEADER_SECURITY_CODE "X-SECURITY-CODE"
